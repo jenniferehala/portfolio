8f3ba431bc34ccea7fae5c1deb265a24d64a3f8f
jehala <jenniferehala@github.com>
2022-02-08 22:53:38 -0500

    commit

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,7 +8,7 @@ const Resume = ({ data }) => {
         <div key={education.school}>
           <h3>{education.school}</h3>
           <p className="info">
-            {education.degree} <span>&bull;</span>
+            {education.degree} <span></span>
             <em className="date">{education.graduated}</em>
           </p>
           <p>{education.description}</p>
